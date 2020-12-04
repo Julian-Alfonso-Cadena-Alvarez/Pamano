871e15baf625a054451f4df5bc322c21b34a61f9
Julian Cadena <julianalfonso11.jc@gmail.com>
2020-12-03 21:21:31 -0500

    Creacion del datatable en producto

--- src/Pamano.Web/Controllers/HomeController.cs
+++ src/Pamano.Web/Controllers/HomeController.cs
@@ -32,10 +32,6 @@ namespace Pamano.Web.Controllers
         {
             return View();
         }
-        public IActionResult DatosCliente()
-        {
-            return View();
-        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
